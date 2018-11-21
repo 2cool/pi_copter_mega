6a434d3798e0e4efbfe0f1dc8f4d96432593e2ba
2cool igor <tcoolcpp@outlook.com>
2018-11-22 01:21:04 +0200

    2018_11_22__ 1_21_04

--- 2git.bat
+++ 2git.bat
@@ -1,1 +1,2 @@
-git add . && git commit -m next_update && git push origin master
+set datetimef=%date:~-4%_%date:~3,2%_%date:~0,2%__%time:~0,2%_%time:~3,2%_%time:~6,2%
+git add . && git commit -m "%datetimef%" && git push origin master
